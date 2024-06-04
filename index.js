a4d3869849a8875d110cdd68119dd8bf9ee04414
Aftab Khan <aftabsaboorkhan@gmail.com>
2024-06-03 23:55:05 -0400

    Url fixed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://file-transfer-app-53886610896f.herokuapp.com/";
+const baseURL = "https://file-transfer-app-53886610896f.herokuapp.com";
 // const baseURL = "http://127.0.0.1:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
