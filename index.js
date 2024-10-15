850879860201525b7862b9ba620728ce9a60285e
Aftab Khan <aftabsaboorkhan@gmail.com>
2024-10-14 23:53:26 -0400

    Changed URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://file-transfer-1-fucd.onrender.com";
+const baseURL = "https://file-transfer-87e6.onrender.com";
 // const baseURL = "http://127.0.0.1:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
